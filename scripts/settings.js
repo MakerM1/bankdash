b6cfe9b86dcbd1aeae4fa680f805c1ae943bc0d6
MakerM1 <132217491+MakerM1@users.noreply.github.com>
2024-01-16 23:11:29 +0400

    test

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,4 +1,5 @@
 const navItem = document.querySelectorAll('.settings__nav-item')
+const navContent1 = document.getElementById('settings__edit-profile')
 
 navItem.forEach((item) => {
     item.addEventListener('click', () => {
@@ -10,6 +11,12 @@ navItem.forEach((item) => {
             if (!navItem[i].classList.contains('active')) {
                 item.classList.add('active')
             }
+        }
+
+        if (navItem[0].classList.contains('active')) {
+            navContent1.style.display = 'flex'
+        } else {
+            navContent1.style.display = 'none'
         }
     })
 })
