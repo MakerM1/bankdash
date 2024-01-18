839840379e674855295a1ae8a58d4c1971981e37
MakerM1 <132217491+MakerM1@users.noreply.github.com>
2024-01-18 14:33:52 +0400

    settings page

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,5 +1,8 @@
 const navItem = document.querySelectorAll('.settings__nav-item')
 const navContent1 = document.getElementById('settings__edit-profile')
+const navContent2 = document.getElementById('settingsPreferences') 
+const navContent3 = document.getElementById('settingsSecurity')
+const saveButton = document.getElementById('settingsSave')
 
 navItem.forEach((item) => {
     item.addEventListener('click', () => {
@@ -15,8 +18,19 @@ navItem.forEach((item) => {
 
         if (navItem[0].classList.contains('active')) {
             navContent1.style.display = 'flex'
-        } else {
+            navContent2.style.display = 'none'
+            navContent3.style.display = 'none'
+            saveButton.setAttribute('form', 'settings__form-1') 
+        } else if (navItem[1].classList.contains('active')) {
             navContent1.style.display = 'none'
+            navContent2.style.display = 'flex'
+            navContent3.style.display = 'none'
+            saveButton.setAttribute('form', 'settings__form-2')
+        }else if (navItem[2].classList.contains('active')) {
+            navContent1.style.display = 'none'
+            navContent2.style.display = 'none'
+            navContent3.style.display = 'block'
+            saveButton.setAttribute('form', 'settings__form-3')
         }
     })
 })
